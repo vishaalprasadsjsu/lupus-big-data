9d4a7e1067938130f6f808aad0899320b9b85c75
Vishaal Prasad <vishaal56@gmail.com>
2018-04-25 20:13:14 -0700

    ml notebook now uses second batch of classified tweets

--- twitter_python/download_and_show_new_count.sh
+++ twitter_python/download_and_show_new_count.sh
@@ -1,26 +1,27 @@
 
 # get the tweets 
 echo "Downloading tweets"
-python2 twython_sqlite.py
+python2 ./twython_sqlite.py
 echo "Done getting tweets"
 
 # show updated count 
 echo "Updated total count:"
-sqlite3 en_lupus.sqlite "select count(*) from hashtags"
+sqlite3 ./en_lupus.sqlite "select count(*) from hashtags"
 
 # show updated count original tweets only 
 echo "Updated total count (original tweets only):"
-sqlite3 en_lupus.sqlite "select count(*) from hashtags where hashtags.retweeted_status = 0"
+sqlite3 ./en_lupus.sqlite "select count(*) from hashtags where hashtags.retweeted_status = 0"
 
 echo "updating analysis CSVs"
 
 echo "updating count for each day of week"
-sqlite3 en_lupus.sqlite < ../../sql-analysis/tweet_count_day_per_week.sql 
+sqlite3 en_lupus.sqlite < ../sql-analysis/tweet_count_day_per_week.sql 
 
 echo "updating count for each hour of day"
-sqlite3 en_lupus.sqlite < ../../sql-analysis/tweet_count_hour_of_day.sql
+sqlite3 en_lupus.sqlite < ../sql-analysis/tweet_count_hour_of_day.sql
 
 echo "saving original tweet content to tweet_content"
 sqlite3 en_lupus.sqlite "select content from hashtags where retweeted_status like '' " > tweet_content.txt
 
 echo "done"
+
